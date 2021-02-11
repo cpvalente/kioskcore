98b4f1aa2994b2b432983cc7a55fda3a1501b968
cv <34649812+cpvalente@users.noreply.github.com>
2021-02-11 15:07:36 +0100

    add some fake data

--- src/features/dashboard/dashboard.jsx
+++ src/features/dashboard/dashboard.jsx
@@ -1,18 +1,82 @@
 export default function Dashboard() {
+  const data = {
+    general: {
+      label: 'MyCueCore',
+      uptime: '7d 4h 26m',
+      firmwareversion: '1.38',
+    },
+    network: {
+      ipaddress: '192.168.1.14',
+      subnetmask: '255.255.255.0',
+    },
+    time: {
+      date: '2020-01-13',
+      time: '23:51:29',
+    },
+    playbacks: {
+      playback1: {
+        name: 'Playback 1',
+        status: 0,
+      },
+      playback2: {
+        name: 'Playback 2',
+        status: 0,
+      },
+      playback3: {
+        name: 'Playback 3',
+        status: 0,
+      },
+      playback4: {
+        name: 'Playback 4',
+        status: 0,
+      },
+      playback5: {
+        name: 'Playback 5',
+        status: 0,
+      },
+      playback6: {
+        name: 'Playback 6',
+        status: 0,
+      },
+    },
+    receiving: {
+      dmxA: {
+        status: 0,
+      },
+      dmxB: {
+        status: 0,
+      },
+      MIDI: {
+        status: 0,
+      },
+      ArtNet: {
+        status: 0,
+      },
+      sACN: {
+        status: 0,
+      },
+      TCP: {
+        status: 0,
+      },
+      UDP: {
+        status: 0,
+      },
+      OSC: {
+        status: 0,
+      },
+    },
+  };
 
   return (
     <div className='dashboard'>
       <div class='card card-general'>
-        <h3 className='cardTitle'>General</h3>
+        <h3 className='cardTitle'>{data.general.label}</h3>
         <ul>
-          <li>
-            <span>MyCueCore</span>
-          </li>
           <li>
             <span>Last Seen:</span> <span>-------</span>
           </li>
           <li>
-            <span>Uptime:</span> <span>-------</span>
+            <span>Uptime:</span> <span>{data.general.uptime}</span>
           </li>
         </ul>
       </div>
@@ -20,83 +84,61 @@ export default function Dashboard() {
         <h3 className='cardTitle'>Network</h3>
         <ul>
           <li>
-            <span>Date:</span> <span>-------</span>
+            <span>Date:</span> <span>{data.time.date}</span>
           </li>
           <li>
-            <span>Time:</span> <span>-------</span>
+            <span>Time:</span> <span>{data.time.time}</span>
           </li>
           <li>
-            <span>FW:</span> <span>-------</span>
+            <span>FW:</span> <span>{data.general.firmwareversion}</span>
           </li>
           <li>
-            <span>IP:</span> <span>-------</span>
+            <span>IP:</span> <span>{data.network.ipaddress}</span>
           </li>
           <li>
-            <span>SUB:</span> <span>-------</span>
+            <span>SUB:</span> <span>{data.network.subnetmask}</span>
           </li>
         </ul>
       </div>
-      <div class='card card-inputs'>
-        <h3 className='cardTitle'>Inputs</h3>
-        <span>DMX A</span> <span>-------</span>
-        <span>DMX B</span> <span>-------</span>
-        <span>MIDI</span> <span>-------</span>
-        <span>Art-Net</span> <span>-------</span>
-        <span>sACN</span> <span>-------</span>
-        <span>TCP</span> <span>-------</span>
-        <span>UDP</span> <span>-------</span>
-        <span>OSC</span> <span>-------</span>
+      <div class='card card-inputs indicatorlist'>
+
+        <div className='indicator'>DMX A</div>
+        <div className='indicator'>DMX B</div>
+        <div className='indicator'>MIDI</div>
+        <div className='indicator'>Art-Net</div>
+        <div className='indicator'>sACN</div>
+        <div className='indicator'>TCP</div>
+        <div className='indicator'>UDP</div>
+        <div className='indicator'>OSC</div>
       </div>
-      <div class='card card-playback'>
-        <h3 className='cardTitle'>Playback</h3>
-        <ul>
-          <li>
-            <span>1</span> <span>-------</span>
-          </li>
-          <li>
-            <span>2</span> <span>-------</span>
-          </li>
-          <li>
-            <span>3</span> <span>-------</span>
-          </li>
-          <li>
-            <span>4</span> <span>-------</span>
-          </li>
-          <li>
-            <span>5</span> <span>-------</span>
-          </li>
-          <li>
-            <span>6</span> <span>-------</span>
-          </li>
-        </ul>
+      <div class='card card-playback indicatorlist'>
+
+        <div className='indicator'>{data.playbacks.playback1.name}</div>
+        <div className='indicator'>{data.playbacks.playback2.name}</div>
+        <div className='indicator'>{data.playbacks.playback3.name}</div>
+        <div className='indicator'>{data.playbacks.playback4.name}</div>
+        <div className='indicator'>{data.playbacks.playback5.name}</div>
+        <div className='indicator'>{data.playbacks.playback6.name}</div>
       </div>
       <div class='card card-messages'>
         <h3 className='cardTitle'>Messages</h3>
         <ul>
+
           <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
+            <span>15:04:10</span> <span>127.0.0.1</span> <span>UDP</span>
+            <span>'UDP testing'</span>
           </li>
+
           <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
+            <span>15:04:10</span> <span>127.0.0.1</span> <span>TCP</span>
+            <span>'TCP testing'</span>
           </li>
+
           <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
+            <span>15:04:10</span> <span>127.0.0.1</span> <span>OSC</span>
+            <span>'OSC testing'</span>
           </li>
-          <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
-          </li>
-          <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
-          </li>
-          <li>
-            <span>Time</span> <span>From</span> <span>Type</span>{' '}
-            <span>Data</span>
-          </li>
+
         </ul>
       </div>
       <div class='card card-heatmap'>

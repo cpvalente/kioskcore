c4f9e243ecaeeed820040444369aa51d3d2c4389
cv <34649812+cpvalente@users.noreply.github.com>
2021-02-12 13:07:46 +0100

    move code

---
+++ src/data/dummyData.js
@@ -0,0 +1,77 @@
+export function getDummyData() {
+  return [
+    {
+      gen: {
+        serial: 'MyCueCore',
+        upt: '7d 4h 26m',
+        fw: '1.38',
+      },
+      ip: {
+        ip: '192.168.1.14',
+        sn: '255.255.255.0',
+      },
+      time: {
+        d: '2020-01-13',
+        t: '23:51:29',
+      },
+      receiving: {
+        d1: '-',
+        d2: '-',
+        midi: 'no',
+        ArtNet: 'no',
+        sACN: 'no',
+        TCP: 'no',
+        UDP: 'no',
+        OSC: 'no',
+      },
+    },
+    {
+      playbacks: [
+        {
+          label: 'Playback 1',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+        {
+          label: 'Playback 2',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+        {
+          label: 'Playback 3',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+        {
+          label: 'Playback 4',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+        {
+          label: 'Playback 5',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+        {
+          label: 'Playback 6',
+          state: 0,
+          cue: '-',
+          list: '-',
+        },
+      ],
+    },
+    {
+      inCount: 162,
+      tcpIn: [],
+    },
+    {
+      outCount: 0,
+      tcpOut: [],
+    },
+  ];
+}

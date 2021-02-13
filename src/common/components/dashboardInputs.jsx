244b80e28313369fdf724cabebb6417b1261003d
cv <34649812+cpvalente@users.noreply.github.com>
2021-02-13 12:34:57 +0100

    general data is component

---
+++ src/common/components/dashboardInputs.jsx
@@ -0,0 +1,65 @@
+export default function DashboardInputs( {data} ) {
+
+  return (
+    <div className='card card-inputs indicatorlist'>
+    <h3 className='cardTitle'>Inputs</h3>
+    <div
+      className={
+        data.receiving.d1 === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      DMX A
+    </div>
+    <div
+      className={
+        data.receiving.d2 === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      DMX B
+    </div>
+    <div
+      className={
+        data.receiving.midi === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      MIDI
+    </div>
+    <div
+      className={
+        data.receiving.a === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      Art-Net
+    </div>
+    <div
+      className={
+        data.receiving.s === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      sACN
+    </div>
+    <div
+      className={
+        data.receiving.t === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      TCP
+    </div>
+    <div
+      className={
+        data.receiving.u === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      UDP
+    </div>
+    <div
+      className={
+        data.receiving.o === 'yes' ? 'indicator active' : 'indicator'
+      }
+    >
+      OSC
+    </div>
+  </div>
+  )
+
+}
--- src/features/dashboard/dashboard.jsx
+++ src/features/dashboard/dashboard.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
+import DashboardGeneral from '../../common/components/dashboardGeneral';
+import DashboardInputs from '../../common/components/dashboardInputs';
 import { getData, getDummyData } from '../../data/dummyData';
 import Heatmap from './heatmap';
 
 export default function Dashboard() {
-  const lastSeen = '11.02.2021 19:59:23';
+
   const [data, setData] = useState(getDummyData);
   const [loading, setLoading] = useState(false);
-
-  console.log(data)
 
   async function getData() {
     Promise.all([
@@ -44,96 +44,8 @@ export default function Dashboard() {
 
   return (
     <div className='dashboard'>
-      <div className='card card-general'>
-        <h3 className='cardTitle'>{data[0].gen.lbl}</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td className='field'>Last seen</td>
-              <td>{lastSeen}</td>
-            </tr>
-            <tr>
-              <td className='field'>Uptime</td>
-              <td>{data[0].gen.upt}</td>
-            </tr>
-            <tr>
-              <td className='field'>Date</td>
-              <td>{data[0].time.d}</td>
-            </tr>
-            <tr>
-              <td className='field'>Time</td>
-              <td>{data[0].time.t}</td>
-            </tr>
-            <tr>
-              <td className='field'>IP Address</td>
-              <td>{data[0].ip.ip}</td>
-            </tr>
-            <tr>
-              <td className='field'>Subnet</td>
-              <td>{data[0].ip.sn}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className='card card-inputs indicatorlist'>
-        <h3 className='cardTitle'>Inputs</h3>
-        <div
-          className={
-            data[0].receiving.d1 === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          DMX A
-        </div>
-        <div
-          className={
-            data[0].receiving.d2 === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          DMX B
-        </div>
-        <div
-          className={
-            data[0].receiving.midi === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          MIDI
-        </div>
-        <div
-          className={
-            data[0].receiving.a === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          Art-Net
-        </div>
-        <div
-          className={
-            data[0].receiving.s === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          sACN
-        </div>
-        <div
-          className={
-            data[0].receiving.t === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          TCP
-        </div>
-        <div
-          className={
-            data[0].receiving.u === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          UDP
-        </div>
-        <div
-          className={
-            data[0].receiving.o === 'yes' ? 'indicator active' : 'indicator'
-          }
-        >
-          OSC
-        </div>
-      </div>
+      <DashboardGeneral data={data[0]}/>
+      <DashboardInputs data={data[0]} />
       <div className='card card-playback indicatorlist'>
         <h3 className='cardTitle'>Playback Status</h3>
         {data[1].playbacks.map((pb) => (
